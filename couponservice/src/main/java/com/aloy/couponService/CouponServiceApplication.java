f2b66fa99bef2260b42797f980ff4c7ea3b79165
Aloy Kumar Das <aloydas@cisco.com>
2021-04-05 18:14:55 +0530

    Added Eureka Clients for Coupon Service and Product Service

--- couponservice/src/main/java/com/aloy/couponService/CouponServiceApplication.java
+++ couponservice/src/main/java/com/aloy/couponService/CouponServiceApplication.java
@@ -2,8 +2,10 @@ package com.aloy.couponService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CouponServiceApplication {
 
     public static void main(String[] args) {
