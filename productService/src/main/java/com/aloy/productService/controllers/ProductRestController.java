61dbded303e2a5fac449b79152f885b30e5b049d
Aloy Kumar Das <aloydas@cisco.com>
2021-04-07 12:36:42 +0530

    Product Service: Added Fault tolerance using resilience 4j for the api calls

--- productService/src/main/java/com/aloy/productService/controllers/ProductRestController.java
+++ productService/src/main/java/com/aloy/productService/controllers/ProductRestController.java
@@ -4,6 +4,7 @@ import com.aloy.productService.dto.Coupon;
 import com.aloy.productService.model.Product;
 import com.aloy.productService.repos.ProductRepo;
 import com.aloy.productService.restClients.CouponClient;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,10 +22,20 @@ public class ProductRestController {
     private ProductRepo repo;
 
     @PostMapping("/products")
+    @Retry(name = "product-api", fallbackMethod = "handleError")
+//    @Retry(name = "product-api")
     public Product create(@RequestBody Product product) {
         Coupon coupon = couponClient.getCoupon(product.getCouponCode());
         product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
         return repo.save(product);
+    }
+
+// Method signature should be same as the api causing the trouble
+    public Product handleError(Product product, Exception exception) {
+//        Other logic can be added here to handle the exception, Something like trying out other services
+        System.out.println("Inside handle error method");
+        product.setDescription(exception.getMessage());
+        return product;
     }
 
 }
